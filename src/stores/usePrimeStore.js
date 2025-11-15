5c526d41c0b980ca27e84d4231ba8fdedebffbde
loaiattar <loaiattar@gmail.com>
2025-11-15 13:46:58 +0100

    ✨ setting up service and store for PrimeNums

---
+++ src/stores/usePrimeStore.js
@@ -0,0 +1,13 @@
+import { create } from 'zustand';
+
+export const usePrimeStore = create((set) => ({
+    prime: {},
+    isLoading : false,
+
+    setPrime: (n, isPrime)  =>
+        set((state) => ({
+            prime: { ...state.prime, [n]: isPrime },
+        })),
+    setIsLoading: (loading) =>
+        set ({ isLoading: loading}),
+}));
